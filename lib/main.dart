cfaa6655d9a0f1a38f40c7e2f8f275082a23c763
Swapna <swapna@oniro.ooo>
2025-08-03 22:34:57 +0530

    Updated quiz logic and UI

--- lib/main.dart
+++ lib/main.dart
@@ -4,20 +4,20 @@ import 'screens/login_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  
-   await Firebase.initializeApp(
-      options: const FirebaseOptions(
-        apiKey: "AIzaSyChgKMIGVdmPakIdwRaUCZA6hnPd9eFKI8",
-        authDomain: "oniro-ooo.firebaseapp.com",
-        databaseURL: "https://oniro-ooo.firebaseio.com",
-        projectId: "oniro-ooo",
-        storageBucket: "oniro-ooo.firebasestorage.app",
-        messagingSenderId: "710760252767",
-        appId: "1:710760252767:web:56d43dcea8f14a609122ea",
-        measurementId: "G-JYG6PZXZSZ",
-      ),
-    );
-    
+
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+      apiKey: "AIzaSyChgKMIGVdmPakIdwRaUCZA6hnPd9eFKI8",
+      authDomain: "oniro-ooo.firebaseapp.com",
+      databaseURL: "https://oniro-ooo.firebaseio.com",
+      projectId: "oniro-ooo",
+      storageBucket: "oniro-ooo.firebasestorage.app",
+      messagingSenderId: "710760252767",
+      appId: "1:710760252767:web:56d43dcea8f14a609122ea",
+      measurementId: "G-JYG6PZXZSZ",
+    ),
+  );
+
   runApp(MyApp());
 }
 
@@ -26,7 +26,33 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Daily Quiz',
-      theme: ThemeData(primarySwatch: Colors.blue),
+      theme: ThemeData(
+        brightness: Brightness.light,
+        primaryColor: Colors.purple,
+        scaffoldBackgroundColor: Colors.white,
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.deepOrange,
+          foregroundColor: Colors.white,
+          elevation: 4,
+        ),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.teal,
+          primary: Colors.purple,
+          secondary: Colors.amber,
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.teal,
+            foregroundColor: Colors.white,
+            padding: EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+            textStyle: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+          ),
+        ),
+        textTheme: TextTheme(
+          titleLarge: TextStyle(color: Colors.purple, fontSize: 20),
+          bodyMedium: TextStyle(color: Colors.black87),
+        ),
+      ),
       home: LoginPage(),
     );
   }
